809ffa1c24ee17c09982947f317c208dd7261514
NicholasKimCoppola <31417360+NicholasKimCoppola@users.noreply.github.com>
2017-08-29 00:07:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+
 //$output = file_get_contents('https://api.telegram.org/bot443240393:AAFZIp68nwDdB_aXjXdxmvMSiVVhMMlhi9s/GetUpdates');
 $output = json_decode (file_get_contents('php://input'),true);
-
-file_put_contents('logs.txt',$output);
+$id = $output['massage']['chat']['id'];
+file_put_contents('logs.txt',$id);
 ?>
