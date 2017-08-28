b68210931757fe4fdb800b48cd1e986bf1d0b2a9
NicholasKimCoppola <31417360+NicholasKimCoppola@users.noreply.github.com>
2017-08-28 23:56:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-
 //$output = file_get_contents('https://api.telegram.org/bot443240393:AAFZIp68nwDdB_aXjXdxmvMSiVVhMMlhi9s/GetUpdates');
-$output = file_get_contents('php://input');
+$output = json_decode (file_get_contents('php://input'),true);
 
 file_put_contents('logs.txt',$output);
 ?>
