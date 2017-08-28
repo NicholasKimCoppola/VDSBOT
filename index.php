cc224a1354af96cea427dfe84c93332d112242fc
NicholasKimCoppola <31417360+NicholasKimCoppola@users.noreply.github.com>
2017-08-29 00:42:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $output = json_decode (file_get_contents('php://input'),true);
 
 $id = $output['message']['chat']['id'];
 
-file_get_contents('https://api.telegram.org/bot443240393:AAFZIp68nwDdB_aXjXdxmvMSiVVhMMlhi9s/sendMessage?chat_id='.$id.'&text= =)');
+file_get_contents('https://api.telegram.org/bot443240393:AAFZIp68nwDdB_aXjXdxmvMSiVVhMMlhi9s/sendMessage?chat_id='.$id.'&text= Добрый день, я бот платформы MISP, на стадии разработки!');
 
 $text = $output['message']['text'];
 file_put_contents('logs.txt',$text);
