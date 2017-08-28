5010c014e97b205f456e1bfcf589817f2386adea
NicholasKimCoppola <31417360+NicholasKimCoppola@users.noreply.github.com>
2017-08-28 23:41:57 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 //$output = file_get_contents('https://api.telegram.org/bot443240393:AAFZIp68nwDdB_aXjXdxmvMSiVVhMMlhi9s/GetUpdates');
 $output = file_get_contents('php;//input');
 
-file_put_contents('logs.txt');
+file_put_contents('logs.txt',$output);
 ?>
