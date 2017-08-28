b06032bfc8f3decf18094681556881138ea849f3
NicholasKimCoppola <31417360+NicholasKimCoppola@users.noreply.github.com>
2017-08-29 00:22:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 //$output = file_get_contents('https://api.telegram.org/bot443240393:AAFZIp68nwDdB_aXjXdxmvMSiVVhMMlhi9s/GetUpdates');
 $output = json_decode (file_get_contents('php://input'),true);
+
 $id = $output['message']['chat']['id'];
-file_put_contents('logs.txt',$id);
+
+file_get_contents('https://api.telegram.org/bot443240393:AAFZIp68nwDdB_aXjXdxmvMSiVVhMMlhi9s/sendMessage?chat_id='.$id.'&text= =)');
+
+$text = $output['message']['text'];
+file_put_contents('logs.txt',$text);
 ?>
